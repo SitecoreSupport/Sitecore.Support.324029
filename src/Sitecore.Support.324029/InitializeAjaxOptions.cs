0839ba3e759defab630b019a9eeea27aa09e03b0
Dmitry Kolinchuk <dmitry.kolinchuk@sitecore.com>
2019-04-11 12:59:27 +0300

    Original code

---
+++ src/Sitecore.Support.324029/InitializeAjaxOptions.cs
@@ -0,0 +1,50 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.ExperienceForms.Mvc;
+using Sitecore.ExperienceForms.Mvc.Pipelines.RenderForm;
+using Sitecore.Mvc.Extensions;
+using Sitecore.Mvc.Pipelines;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc.Ajax;
+
+namespace Sitecore.Support
+{
+  public class InitializeAjaxOptions : MvcPipelineProcessor<RenderFormEventArgs>
+  {
+    private readonly IFormRenderingContext _formRenderingContext;
+
+    public InitializeAjaxOptions(IFormRenderingContext formRenderingContext)
+    {
+      Assert.ArgumentNotNull(formRenderingContext, "formRenderingContext");
+      _formRenderingContext = formRenderingContext;
+    }
+
+    public override void Process(RenderFormEventArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      if (args.ViewModel.IsAjax)
+      {
+        if (args.HtmlHelper.ViewContext.UnobtrusiveJavaScriptEnabled)
+        {
+          AjaxOptions ajaxOptions = new AjaxOptions();
+          ajaxOptions.HttpMethod = "Post";
+          ajaxOptions.InsertionMode = InsertionMode.ReplaceWith;
+          ajaxOptions.UpdateTargetId = args.FormHtmlId;
+          ajaxOptions.OnSuccess = FormattableString.Invariant($"$.validator.unobtrusive.parse('#{args.FormHtmlId}');$.fxbFormTracker.parse('#{args.FormHtmlId}');");
+          AjaxOptions ajaxOptions2 = ajaxOptions;
+          IDictionary<string, object> dictionary = ajaxOptions2.ToUnobtrusiveHtmlAttributes();
+          foreach (string key in dictionary.Keys)
+          {
+            args.Attributes[key] = dictionary[key];
+          }
+        }
+        if (!args.IsPost)
+        {
+          args.QueryString.Add("fxb.FormItemId", args.ViewModel.ItemId.ToGuid());
+          args.QueryString.Add("fxb.HtmlPrefix", _formRenderingContext.Prefix.Trim('.'));
+        }
+        args.RouteName = "FormBuilder";
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.324029/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.324029")]
+[assembly: AssemblyProduct("Sitecore.Support.324029")]
+[assembly: ComVisible(false)]
